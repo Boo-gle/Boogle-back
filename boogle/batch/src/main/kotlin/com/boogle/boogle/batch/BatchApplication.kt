8e6b2d86a8b4bdda6106e71965872eac05014e24
hde <ekdmsdl336@gmail.com>
2026-02-21 09:02:41 +0900

    🔧 chore: JPA Auditing 활성화 및 컴포넌트 스캔 설정 정리

--- boogle/batch/src/main/kotlin/com/boogle/boogle/batch/BatchApplication.kt
+++ boogle/batch/src/main/kotlin/com/boogle/boogle/batch/BatchApplication.kt
@@ -1,12 +1,14 @@
-package com.boogle.boogle
+package com.boogle.boogle.batch
 
 import com.boogle.boogle.batch.config.BatchBootstrapConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@SpringBootApplication(scanBasePackages = ["com.boogle.boogle"])
+@SpringBootApplication
 @Import(BatchBootstrapConfig::class)
+@EnableJpaAuditing
 class BatchApplication
 
 fun main(args: Array<String>) {
