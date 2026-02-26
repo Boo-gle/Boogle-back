fd7019ecee76308a58bf511af17c0c9c15e484ad
hde <ekdmsdl336@gmail.com>
2026-02-26 09:49:42 +0900

    🐛 fix: job 실행 후 자동 종료되도록 실행 클래스 수정

--- boogle/batch/src/main/kotlin/com/boogle/boogle/batch/BatchApplication.kt
+++ boogle/batch/src/main/kotlin/com/boogle/boogle/batch/BatchApplication.kt
@@ -1,10 +1,12 @@
 package com.boogle.boogle.batch
 
 import com.boogle.boogle.batch.config.BatchBootstrapConfig
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 @Import(BatchBootstrapConfig::class)
@@ -12,5 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 class BatchApplication
 
 fun main(args: Array<String>) {
-    runApplication<BatchApplication>(*args)
+    val ctx = runApplication<BatchApplication>(*args)
+    val exitCode = SpringApplication.exit(ctx)
+    exitProcess(exitCode)
 }
